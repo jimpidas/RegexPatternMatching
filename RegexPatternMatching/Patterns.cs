60f96bcb0b3c0c8e833cda2027a885a06ab0608e
unknown <jimpidas>
2021-05-10 19:54:53 +0530

    [Jyosmita]: Added the Regular expression

---
+++ RegexPatternMatching/Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPatternMatching
+{
+    public class Patterns
+    {
+        public static string REGEX_PINCODE = "^[1-9][0-9]{5}$";
+        
+        public bool validatePinCode(string pincode)
+        {
+            return  Regex.IsMatch(pincode, REGEX_PINCODE);
+        }
+
+    }
+
+}
---
+++ RegexPatternMatching/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RegexPatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns pattern = new Patterns();
+            Console.WriteLine(pattern.validatePinCode("400088"));
+        }
+    }
+}

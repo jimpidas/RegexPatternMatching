6ba6b0b80265a96ddc9f8506eb1961f7ba0f2309
unknown <jimpidas>
2021-05-12 07:20:26 +0530

    [Jyosmita]:Added the code to check for valid email id

--- RegexPatternMatching/Program.cs
+++ RegexPatternMatching/Program.cs
@@ -10,9 +10,14 @@ namespace RegexPatternMatching
             Console.WriteLine("Enter the first name");
             string FName = Console.ReadLine();
             pattern.isValidFirstName(FName);
+
             Console.WriteLine("Enter last name");
             string LName = Console.ReadLine();
             pattern.isValidLastName(LName);
+
+            Console.WriteLine("Enter email");
+            string Email = Console.ReadLine();
+            pattern.isValidLastEmail(Email);
               
         }
     }
